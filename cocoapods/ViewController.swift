4a1ca713b9c142583e1886c72b6fb6e0ac6d3b68
alangogoro <cayne9160@gmail.com>
2019-11-19 14:22:54 +0800

    安安安安安

--- cocoapods/ViewController.swift
+++ cocoapods/ViewController.swift
@@ -9,12 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let isNew: Bool = true
+    let str: String = "GitHub test"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
 
